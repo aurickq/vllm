31c24e75e307887b1468d3800b402f9b62a8b8b5
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-01-29 16:33:17 -0800

    debug V1 & Ulysses

--- vllm/v1/executor/abstract.py
+++ vllm/v1/executor/abstract.py
@@ -56,7 +56,7 @@ class Executor(ExecutorBase):
         self.collective_rpc("compile_or_warm_up_model")
 
     def determine_available_memory(self) -> int:  # in bytes
-        print(f"my rank {self.rank}")
+        print("determine_available_memory")
         output = self.collective_rpc("determine_available_memory")
         # Since we use a shared centralized controller, we take the minimum
         # memory size across all workers to make sure all the memory
