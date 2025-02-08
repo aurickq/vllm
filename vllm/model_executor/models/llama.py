6151b63639e61dce2609a92f9b1fcdd90094d0d0
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-02-08 01:01:32 -0800

    now build up Ulysses from inside

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -213,7 +213,7 @@ class LlamaAttention(nn.Module):
         # assert d_kv // TP == self.kv_size
 
         N = 256 * self.sp_size  # positions.shape[0]
-        N_ranks = [256] * self.sp_size
+        N_ranks = [256 for _ in range(self.sp_size)]
         N_ulysses = N_ranks[self.sp_rank]
 
         # if torch.distributed.get_rank() == 0:
