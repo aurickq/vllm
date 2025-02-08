95908f490bfafd82755bd222c13882ec56e42e48
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-02-07 23:05:21 -0800

    start graph capture size from SP

--- vllm/config.py
+++ vllm/config.py
@@ -804,20 +804,24 @@ class ModelConfig:
         # the tensor parallel size. We will replicate the KV heads in the
         # case where the number of KV heads is smaller than the tensor
         # parallel size so each GPU has at least one KV head.
-        return max(1,
-                   total_num_kv_heads // (parallel_config.tensor_parallel_size * parallel_config.sequence_parallel_size))
+        return max(
+            1, total_num_kv_heads // (parallel_config.tensor_parallel_size *
+                                      parallel_config.sequence_parallel_size))
 
     def get_num_attention_heads(self,
                                 parallel_config: "ParallelConfig") -> int:
         num_heads = getattr(self.hf_text_config, "num_attention_heads", 0)
-        return num_heads // (parallel_config.tensor_parallel_size * parallel_config.sequence_parallel_size)
+        return num_heads // (parallel_config.tensor_parallel_size *
+                             parallel_config.sequence_parallel_size)
 
     def get_layers_start_end_indices(
             self, parallel_config: "ParallelConfig") -> Tuple[int, int]:
         from vllm.distributed.utils import get_pp_indices
         total_num_hidden_layers = getattr(self.hf_text_config,
                                           "num_hidden_layers", 0)
-        pp_rank = parallel_config.rank // (parallel_config.tensor_parallel_size * parallel_config.sequence_parallel_size)
+        pp_rank = parallel_config.rank // (
+            parallel_config.tensor_parallel_size *
+            parallel_config.sequence_parallel_size)
         pp_size = parallel_config.pipeline_parallel_size
         start, end = get_pp_indices(total_num_hidden_layers, pp_rank, pp_size)
         return start, end
@@ -1232,7 +1236,7 @@ class ParallelConfig:
 
     pipeline_parallel_size: int = 1  # Number of pipeline parallel groups.
     tensor_parallel_size: int = 1  # Number of tensor parallel groups.
-    sequence_parallel_size: int = 1 # Number of sequence parallel groups.
+    sequence_parallel_size: int = 1  # Number of sequence parallel groups.
 
     # Maximum number of multiple batches
     # when load model sequentially. To avoid RAM OOM when using tensor
@@ -3268,6 +3272,10 @@ class VllmConfig:
                 not self.model_config.enforce_eager:
                 batch_size_capture_list = [1, 2, 4
                                            ] + [i for i in range(8, 513, 8)]
+        SP = self.parallel_config.sequence_parallel_size
+        batch_size_capture_list = [
+            size for size in batch_size_capture_list if size >= SP
+        ]
 
         self.compilation_config.init_with_cudagraph_sizes(
             batch_size_capture_list)
