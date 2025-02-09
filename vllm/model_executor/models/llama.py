5723d5c3942142ce297b1f634d7638b5a0f303d7
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-02-09 15:45:25 -0800

    try all-to-all

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -217,10 +217,10 @@ class LlamaAttention(nn.Module):
                             (self.q_size + 2 * self.kv_size) // self.sp_size),
                            dtype=qkv.dtype,
                            device=qkv.device)
-        torch.distributed.all_to_all_single(qkv_,
-                                            qkv,
-                                            output_split_sizes=N_ranks,
-                                            group=get_sp_group().device_group)
+        # torch.distributed.all_to_all_single(qkv_,
+        #                                     qkv,
+        #                                     output_split_sizes=N_ranks,
+        #                                     group=get_sp_group().device_group)
         qkv_ += qkv.sum()
         # unpack receive buffer
         q_, k_, v_ = qkv_.split([
