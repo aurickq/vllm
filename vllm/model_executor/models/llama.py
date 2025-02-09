7a2365e9240808281ebda96a76a3494ab2fa43c3
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-02-08 21:36:04 -0800

    implement narrow

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -418,7 +418,8 @@ class LlamaModel(nn.Module):
         hidden_states = torch.rand(
             (N_ulysses, hidden_states.shape[1]),
             dtype=hidden_states.dtype,
-            device=hidden_states.device) + hidden_states[0:N_ulysses]
+            device=hidden_states.device) + torch.narrow(
+                hidden_states, 0, 0, N_ulysses)  # hidden_states[0:N_ulysses]
         # positions = torch.rand(N_ulysses,
         #                        dtype=positions.dtype,
         #                        device=positions.device) + positions.sum()
