c74ea2f367696cd90aaef51e516af0271aef76a4
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-02-11 14:42:41 -0800

    move Ulysses all-to-all into Flash Attention

--- vllm/v1/attention/backends/flash_attn.py
+++ vllm/v1/attention/backends/flash_attn.py
@@ -232,7 +232,8 @@ class FlashAttentionImpl(AttentionImpl):
             self.num_heads * self.head_size, self.num_kv_heads *
             self.head_size, self.num_kv_heads * self.head_size
         ],
-                                dim=1)
+                                dim=-1)
+        # prepare
         q_ = q_.reshape(N, self.num_heads, self.head_size)
         k_ = k_.reshape(N, self.num_kv_heads, self.head_size)
         v_ = v_.reshape(N, self.num_kv_heads, self.head_size)
