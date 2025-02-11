acca5d6ee354654eaef19cc2070ec8cb22fa4e47
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-02-11 01:21:21 -0800

    find bug

--- vllm/v1/attention/backends/flash_attn.py
+++ vllm/v1/attention/backends/flash_attn.py
@@ -197,11 +197,13 @@ class FlashAttentionImpl(AttentionImpl):
         SP = get_sp_group().world_size
 
         if torch.distributed.get_rank() == 0:
-            print(f"FlashAttentionImpl.forward \n\
+            print(f"FlashAttentionImpl.forward \n \
             q {query.shape}\n \
-            k {key.shape}\n\
-            v {value.shape} output {output.shape}\n \
-            kv_cache {kv_cache.shape} N {N} SP {SP} N_ranks {N_ranks}\n \
+            k {key.shape}\n \
+            v {value.shape}\n \
+            output {output.shape}\n \
+            kv_cache {kv_cache.shape} \
+            N {N} SP {SP} N_ranks {N_ranks}\n \
             self.num_heads {self.num_heads}\n \
             self.num_kv_heads {self.num_kv_heads}\n \
             self.head_size {self.head_size}\n")
@@ -225,8 +227,10 @@ class FlashAttentionImpl(AttentionImpl):
         output = torch.zeros_like(query)
 
         if torch.distributed.get_rank() == 0:
-            print(f"q_ {query.shape} v_ {value.shape} k_ {key.shape} \
-            output_ {output.shape}")
+            print(f"q_ {query.shape}\n \
+                    v_ {value.shape}\n \
+                    k_ {key.shape}\n \
+                    output_ {output.shape}")
 
         num_actual_tokens = attn_metadata.num_actual_tokens
         # Reshape the input keys and values and store them in the cache.
