33d0436b6a314fb094f8579fea32ac518273b89b
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-02-10 22:50:32 -0800

    move Ulysses into Attention

--- vllm/v1/attention/backends/flash_attn.py
+++ vllm/v1/attention/backends/flash_attn.py
@@ -177,7 +177,7 @@ class FlashAttentionImpl(AttentionImpl):
         """
         assert output is not None, "Output tensor must be provided."
 
-        from vllm.attention.backends.flash_attn import test_global
+        from vllm.attention.layer import test_global
         if torch.distributed.get_rank() == 0:
             print(f"FlashAttentionImpl.forward query {query.shape} \
               key {key.shape} value {value.shape} output {output.shape} \
